1ce317f18e970bc91124415798c6947f275ecfc7
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-04-28 12:25:36 -0500

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-if __name__=="__main__":
+if __name__ == '__main__':
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
