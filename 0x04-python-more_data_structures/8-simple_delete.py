84f43c1356c3c4c9da49acf53d92b3ed3fc1aac0
Blacksterhack <blastertv1001@gmail.com>
2021-05-06 18:50:56 -0500

    simple_delete

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if (key not in a_dictionary):
+        return (a_dictionary)
+    else:
+        del a_dictionary[key]
+    return (a_dictionary)
