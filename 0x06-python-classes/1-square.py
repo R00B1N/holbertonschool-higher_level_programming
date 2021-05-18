10bf4a90287a86397c46feaad8de87548a225e4a
Blacksterhack <blastertv1001@gmail.com>
2021-05-18 12:28:08 -0500

    1square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """ Square class defines a square by size (private) """
+    def __init__(self, size):
+        """ initializes the object """
+        self.__size = size
