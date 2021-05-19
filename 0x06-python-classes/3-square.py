d2e9368df6efc9b3d49cc7f0ff0681ca152aa66e
Blacksterhack <blastertv1001@gmail.com>
2021-05-18 23:41:01 -0500

    3square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
+
+
 class Square:
-    __size = 0
+    '''Class Square
+    '''
+    pass
 
     def __init__(self, size=0):
-        if isinstance(size, int):
-                self.__size = size
-        else:
+        '''init method of class Square
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        return self.__size ** 2
+        return self.__size**2
