c1406840981fc1687e7a4f3ea8bc71f32f8c852d
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-05-18 23:49:16 -0500

    5square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
+"""square class"""
+
+
 class Square:
+    """what are these for? who knows"""
     def __init__(self, size=0):
+        self.size = size
+    """getter size"""
+    @property
+    def size(self):
+        return self.__size
+    """ setter size """
+    @size.setter
+    def size(self, size):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
+        self.__size = size
+    """return the square's area"""
     def area(self):
-        return self.__size ** 2
-
+        return self.__size * self.__size
+    """print square"""
     def my_print(self):
-        if self.__size == 0:
+        for s in range(self.__size):
+            print("#" * self.__size)
+        if self.__size is 0:
             print()
-        else:
-            for i in range(0, self.__size):
-                for j in range(0, self.__size):
-                    print("#", end="")
-                print()
