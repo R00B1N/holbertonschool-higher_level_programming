3ad4b92815fd3c599eea71551081310fdc94af5f
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-05-18 23:44:34 -0500

    squaring-third

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
+"""square"""
 
 
 class Square:
-    '''Class Square
-    '''
-    pass
-
+    """what are these for? who knows"""
     def __init__(self, size=0):
-        '''init method of class Square
-        '''
-        self.size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, size):
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
+    """return the square's area"""
     def area(self):
-        return self.__size**2
+        return self.__size * self.__size
