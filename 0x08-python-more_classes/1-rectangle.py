973df7dd522f5e3a128a73e8acddfcf17957af6f
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-05-20 18:22:52 -0500

    1rectangle-fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,20 +3,24 @@
 module contains one class
 """
 
+
 class Rectangle:
     """ class Rectangle """
     def __init__(self, width=0, height=0):
         """ instantiation """
         self.height = height
         self.width = width
+
     @property
     def width(self):
         """ getter for width """
         return self.__width
+
     @property
     def height(self):
         """ getter for height """
         return self.__height
+
     @width.setter
     def width(self, value):
         """ setter for width """
@@ -25,6 +29,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @height.setter
     def height(self, value):
         """ setter for height """
