373f70cac253a21190c38d614b9cf69a2afb2657
Blackster <74608055+Blacksterhack@users.noreply.github.com>
2021-05-20 18:24:18 -0500

    2rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
+"""
+module contains one class
+"""
+
 
 class Rectangle:
+    """ class Rectangle """
     def __init__(self, width=0, height=0):
+        """ instantiation """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ getter for width """
         return self.__width
 
     @property
     def height(self):
+        """ getter for height """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """ setter for width """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,6 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """ setter for height """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -30,9 +40,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """ returns area of the rectangle """
         return self.__height * self.__width
 
     def perimeter(self):
+        """ returns perimeter of the rectangle """
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__height + self.__width) * 2
