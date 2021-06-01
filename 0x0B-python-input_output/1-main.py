4a7aa9d7b56fd4e0289f0eeeae4683847452b218
Blacksterhack <blastertv1001@gmail.com>
2021-06-01 17:45:49 -0500

    1main

--- 0x0B-python-input_output/1-main.py
+++ 0x0B-python-input_output/1-main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-write_file = __import__('3-write_file').write_file
+write_file = __import__('1-write_file').write_file
 
 nb_characters = write_file("my_first_file.txt", "Holberton School is so cool!\n")
 print(nb_characters)
-write_file("my_first_file.txt", "Holberton School is so cooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooool!\n")
-with open("my_first_file.txt") as f:
-    print(f.read(), end="")
