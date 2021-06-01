a06aee60c1b2d1f1db134d77a5ea2028bbbf7fe7
Blacksterhack <blastertv1001@gmail.com>
2021-06-01 18:37:40 -0500

    8-class_to_json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains one function
+"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description
+    with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    """
+    return obj.__dict__
