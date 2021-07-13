1477b04c8898869cd0c74f00a4f293d3fe8fccac
Blacksterhack <blastertv1001@gmail.com>
2021-07-13 15:15:19 -0500

    no_link

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- a script that lists all records of the table second_table
+-- in the current database in the MySQL server
+SELECT score, name FROM second_table WHERE name <> "" ORDER BY score DESC-- a script that lists all records of the table second_table
+-- in the current database in the MySQL server
+SELECT score, name FROM second_table WHERE name <> "" ORDER BY score DESC
