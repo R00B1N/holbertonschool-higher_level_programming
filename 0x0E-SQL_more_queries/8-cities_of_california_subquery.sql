d7699482e72919f15e791334f23f1d75252e5928
Blacksterhack <blastertv1001@gmail.com>
2021-07-13 21:56:44 -0500

    californiawq

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- a script that lists all the cities of California state
+-- that can be found in the database hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
