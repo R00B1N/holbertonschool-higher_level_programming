2f51c04555589bd6c85e429e0fba08fe55398a86
Blacksterhack <blastertv1001@gmail.com>
2021-07-14 23:31:01 -0500

    count_shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- a script that lists all genres contained in hbtn_0d_tvshows
+-- and displays the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;
