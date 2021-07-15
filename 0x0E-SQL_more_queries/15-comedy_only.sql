d85e7d7711e876e0de309080f4f174c147d971dc
Blacksterhack <blastertv1001@gmail.com>
2021-07-14 23:38:23 -0500

    comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,3 @@
+-- a script that lists all Comedy shows
+-- contained in hbtn_0d_tvshows
+SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
