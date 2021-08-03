114239664a3c5068acb123d595af0c6a4e8ada9f
Blacksterhack <blastertv1001@gmail.com>
2021-08-02 21:56:46 -0500

    multi_language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const C = 'C is fun';
-const Python = 'Python is cool';
-const Javascript = 'JavaScript is amazing';
-console.log(C+"\n"+Python+"\n"+Javascript);
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
