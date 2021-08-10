52279e3bcac549306da49c5f288a63fa33ee16d9
Blacksterhack <blastertv1001@gmail.com>
2021-08-09 21:59:26 -0500

    fixing_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
+"""
+accepts 3 arguments (mysql username, password and database name)
+and lists all states from that database
+"""
 import sys
 import MySQLdb
 
 
 def main(argv):
+    """connects to a given mysql database and lists all 'states' from it"""
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
