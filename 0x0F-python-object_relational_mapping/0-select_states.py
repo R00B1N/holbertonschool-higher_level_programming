f9a4b5531c34e9c05b8ec7624def8bc3b9ac4417
Blacksterhack <blastertv1001@gmail.com>
2021-08-09 21:48:29 -0500

    selecting_states_file_py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+
+def main(argv):
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        main(sys.argv)
