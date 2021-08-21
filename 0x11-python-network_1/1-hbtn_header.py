ae9005ea84cadf9047b25f9f294fb24bf8d00896
Blacksterhack <blastertv1001@gmail.com>
2021-08-20 21:21:51 -0500

    hbtn_header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a Python script that displays a header value """
+import urllib.request
+import sys
+
+
+if __name__ == '__main__':
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.info()['X-Request-Id'])#!/usr/bin/python3
+        """ a Python script that displays a header value """
+        import urllib.request
+        import sys
+
+
+        if __name__ == '__main__':
+                req = urllib.request.Request(sys.argv[1])
+                    with urllib.request.urlopen(req) as response:
+                                print(response.info()['X-Request-Id'])
