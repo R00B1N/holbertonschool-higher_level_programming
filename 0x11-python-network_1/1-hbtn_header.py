113e4c373df0172a336a966975d92f5506532285
Blacksterhack <blastertv1001@gmail.com>
2021-08-20 21:23:01 -0500

    hbtn_header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,13 +7,4 @@ import sys
 if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
-        print(response.info()['X-Request-Id'])#!/usr/bin/python3
-        """ a Python script that displays a header value """
-        import urllib.request
-        import sys
-
-
-        if __name__ == '__main__':
-                req = urllib.request.Request(sys.argv[1])
-                    with urllib.request.urlopen(req) as response:
-                                print(response.info()['X-Request-Id'])
+        print(response.info()['X-Request-Id'])
